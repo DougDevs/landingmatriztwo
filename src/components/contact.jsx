18b8b3f04bccfe2716c91cf543a643e6388025d6
Douglas Ryan Terleski <103542806+DougDevs@users.noreply.github.com>
2023-08-03 19:02:34 -0300

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -149,7 +149,7 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
+            Desenvolvido por SC DataLink {" "}
             <a href="http://www.templatewire.com" rel="nofollow">
               TemplateWire
             </a>
